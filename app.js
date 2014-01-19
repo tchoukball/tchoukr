b91aa5e299192a8b038396bcdc3db250eca5557a
Nathanaël Khodl <nkhodl@gmail.com>
2014-01-19 12:31:46 +0100

    change app template to jade

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
+var testcomponent = require('./routes/testcomponent');
 var http = require('http');
 var path = require('path');
 
@@ -14,7 +14,7 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -32,7 +32,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/component', testcomponent.test);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/testcomponent.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.test = function(req, res){
+  res.send("Here is the component");
+};
