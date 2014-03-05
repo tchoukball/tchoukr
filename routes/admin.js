000e7a33bef88a332c52b9d0fbbb10d5095b41cf
Nathanaël Khodl <nkhodl@gmail.com>
2014-03-05 18:48:29 +0100

    add club creation

--- routes/admin.js
+++ routes/admin.js
@@ -21,7 +21,24 @@ exports.clubs = function(req, res){
         if(req.body.newclub){
             var clubName = req.body.newclub ;
             db.tables.clubs.findOne({name:clubName},function(err,club){
-                displayResult();
+
+                if(club){
+                    res.end("Club already exists");
+                }else{
+                    var newClub  = new db.tables.clubs({
+                        name: clubName
+                    });
+
+                    newClub.validate(function(err){
+                        if(err){
+                            res.end('Wrong name')
+                        }else{
+                            newClub.save();
+                            displayResult();
+                        }
+                    })
+                }
+
             });
         }else
             displayResult();
